3c9a77a6fbf7f35a78c410c014c4264723d7d3d9
Eingerjar <cksrb1526@naver.com>
2022-09-29 21:51:05 +0900

    fix: norm check

--- check_identifier.c
+++ check_identifier.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_identifier.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgim <cgim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 21:47:41 by cgim              #+#    #+#             */
+/*   Updated: 2022/09/29 21:50:37 by cgim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 
-int	fstrncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
@@ -11,23 +23,24 @@ int	fstrncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-int check_identifier(char *type) {
-    size_t  n;
+int	check_identifier(char *type)
+{
+	size_t	n;
 
-    n = ft_strlen(type);
-    if (type == 0)
-        return (-1);
-    if (ft_strncmp("A", type, n) == 0)
-        return (0);
-    else if (ft_strncmp("C", type, n) == 0)
-        return (1);
-    else if (ft_strncmp("L", type, n) == 0)
-        return (2);
-    else if (ft_strncmp("sp", type, n) == 0)
-        return (3);
-    else if (ft_strncmp("pl", type, n) == 0)
-        return (4);
-    else if (ft_strncmp("cy", type, n) == 0)
-        return (5);
-    return (-1);
+	n = ft_strlen(type);
+	if (type == 0)
+		return (-1);
+	if (ft_strncmp("A", type, n) == 0)
+		return (0);
+	else if (ft_strncmp("C", type, n) == 0)
+		return (1);
+	else if (ft_strncmp("L", type, n) == 0)
+		return (2);
+	else if (ft_strncmp("sp", type, n) == 0)
+		return (3);
+	else if (ft_strncmp("pl", type, n) == 0)
+		return (4);
+	else if (ft_strncmp("cy", type, n) == 0)
+		return (5);
+	return (-1);
 }
