9907dee917b2089ab70ddd0682b34a16767c91bf
Eingerjar <cksrb1526@naver.com>
2022-09-29 21:36:30 +0900

    add: check_identifier

---
+++ check_identifier.c
@@ -0,0 +1,33 @@
+#include "parser.h"
+
+int	fstrncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < n && (s1[i] || s2[i]))
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
+}
+
+int check_identifier(char *type) {
+    size_t  n;
+
+    n = ft_strlen(type);
+    if (type == 0)
+        return (-1);
+    if (ft_strncmp("A", type, n) == 0)
+        return (0);
+    else if (ft_strncmp("C", type, n) == 0)
+        return (1);
+    else if (ft_strncmp("L", type, n) == 0)
+        return (2);
+    else if (ft_strncmp("sp", type, n) == 0)
+        return (3);
+    else if (ft_strncmp("pl", type, n) == 0)
+        return (4);
+    else if (ft_strncmp("cy", type, n) == 0)
+        return (5);
+    return (-1);
+}
