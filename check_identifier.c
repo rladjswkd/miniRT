086cbb1c4a4a7bb483355e8488f3afe7cd4bb9bd
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-09-30 16:49:02 +0900

    modified: check_identifier.c string_utils.[ch]

--- check_identifier.c
+++ check_identifier.c
@@ -11,18 +11,7 @@
 /* ************************************************************************** */
 
 #include "parser.h"
-#include <stdlib.h>
 #include "string_utils.h"
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = -1;
-	while (++i < n && (s1[i] || s2[i]))
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-	return (0);
-}
 
 int	check_identifier(char *type)
 {
