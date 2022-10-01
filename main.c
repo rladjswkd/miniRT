a56b9e486e6559179b96de2693f9333f2a12c634
rladjswkd <dlfmaqkrrudah@gmail.com>
2022-10-01 15:10:23 +0900

    done: parsing file contents

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "parser_util_split.h"
 #include <math.h>
 #include "converter.h"
-int	check_comma_cnt(char *s); // should be removed.
+#include "checker.h"
 
 typedef struct s_rgb
 {
@@ -66,6 +66,22 @@ typedef struct s_cylinder
 	double			height;
 	t_rgb			rgb;
 }	t_cylinder;
+
+typedef struct s_node
+{
+	void			*data;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_rt_info
+{
+	t_ambient	a;
+	t_camera	c;
+	t_light		l;
+	t_node		*sp;
+	t_node		*cy;
+	t_node		*pl;
+}	t_rt_info;
 
 int	check_rgb(t_rgb rgb)
 {
@@ -137,144 +153,242 @@ int	set_coordinate(char *coord_str, t_coordinate *coord)
 	return (free_splitted(coord_info, 1));
 }
 
-int	set_ambient(char **info, t_ambient *a) // caller must check whether the count of splitted is 3. and this function is called only if splitted[0] is "A"
+int	set_ambient(char **info, int cnt, t_rt_info *rt_info)
 {
-	double	intensity;
-	t_rgb	rgb;
+	double		intensity;
+	t_ambient	*a;
 
-	if (!get_double(info[1], &intensity)
-		|| intensity < 0 || 1 < intensity)
+	if (cnt != 3)
 		return (0);
-	if (!set_rgb(info[2], &rgb))
+	a = &(rt_info->a);
+	if (!get_double(info[1], &intensity) || intensity < 0 || 1 < intensity)
+		return (0);
+	if (!set_rgb(info[2], &(a->rgb)))
 		return (0);
 	a->intensity = intensity;
-	a->rgb.r = rgb.r;
-	a->rgb.g = rgb.g;
-	a->rgb.b = rgb.b;
 	return (1);
 }
 
-int	set_light(char **info, t_light *l)// caller must check whether the count of splitted is 4. and this function is called only if splitted[0] is "L"
+int	set_light(char **info, int cnt, t_rt_info *rt_info)
 {
-	t_coordinate	coordinate;
-	double			intensitiy;
-	t_rgb			rgb;
+	double	intensitiy;
+	t_light	*l;
 
-	if (!set_coordinate(info[1], &coordinate))
+	if (cnt != 4)
 		return (0);
-	if (!get_double(info[2], &intensitiy)
-		|| intensitiy < 0 || 1 < intensitiy)
+	l = &(rt_info->l);
+	if (!set_coordinate(info[1], &(l->coordinate)))
 		return (0);
-	if (!set_rgb(info[3], &rgb))
+	if (!get_double(info[2], &intensitiy) || intensitiy < 0 || 1 < intensitiy)
 		return (0);
-	l->coordinate.x = coordinate.x;
-	l->coordinate.y = coordinate.y;
-	l->coordinate.z = coordinate.z;
+	if (!set_rgb(info[3], &(l->rgb)))
+		return (0);
 	l->intensity = intensitiy;
-	l->rgb.r = rgb.r;
-	l->rgb.g = rgb.g;
-	l->rgb.b = rgb.b;
 	return (1);
 }
 
-
-int	set_camera(char **info, t_camera *c)// caller must check whether the count of splitted is 4. and this function is called only if splitted[0] is "C"
+int	set_camera(char **info, int cnt, t_rt_info *rt_info)
 {
-	t_coordinate	coordinate;
-	t_vector		normalized;
-	int				fov;
+	int			fov;
+	t_camera	*c;
 
-	if (!set_coordinate(info[1], &coordinate))
+	if (cnt != 4)
 		return (0);
-	if (!set_coordinate(info[2], &normalized)
-		|| !check_normal(normalized))
+	c = &(rt_info->c);
+	if (!set_coordinate(info[1], &(c->coordinate)))
 		return (0);
-	if (!get_int(info[3], &fov)
-		|| fov < 0 || 180 < fov) // 180 0
+	if (!set_coordinate(info[2], &(c->normalized))
+		|| !check_normal(c->normalized))
 		return (0);
-	c->coordinate.x = coordinate.x;
-	c->coordinate.y = coordinate.y;
-	c->coordinate.z = coordinate.z;
-	c->normalized.x = normalized.x;
-	c->normalized.y = normalized.y;
-	c->normalized.z = normalized.z;
+	if (!get_int(info[3], &fov)	|| fov < 0 || 180 < fov) // 180 0
+		return (0);
 	c->fov = fov;
 	return (1);
 }
 
-int	set_plane(char **info, t_plane *pl)// caller must check whether the count of splitted is 4. and this function is called only if splitted[0] is "pl"
+t_node	*get_last_node(t_node *list) // this function is called after malloc, so list is non-null.
 {
-	t_coordinate	coordinate;
-	t_vector		normalized;
-  t_rgb			rgb;
-
-	if (!set_coordinate(info[1], &coordinate))
-		return (0);
-  if (!set_coordinate(info[2], &normalized)
-	|| !check_normal(normalized))
-		return (0);
-	if (!set_rgb(info[3], &rgb))
-		return (0);
-	pl->coordinate.x = coordinate.x;
-	pl->coordinate.y = coordinate.y;
-	pl->coordinate.z = coordinate.z;
-	pl->normalized.x = normalized.x;
-	pl->normalized.y = normalized.y;
-	pl->normalized.z = normalized.z;
-	pl->rgb.r = rgb.r;
-	pl->rgb.g = rgb.g;
-	pl->rgb.b = rgb.b;
-  return (1);
+	while (list->next)
+		list = list->next;
+	return (list);
 }
 
-int	set_sphere(char **info, t_sphere *sp)// caller must check whether the count of splitted is 4. and this function is called only if splitted[0] is "sp"
+int	set_plane(char **info, int cnt, t_rt_info *rt_info)
 {
-	t_coordinate	coordinate;
-	double			diameter;
-  t_rgb			rgb;
+	t_plane	*pl;
 
-	if (!set_coordinate(info[1], &coordinate))
+	if (cnt != 4)
 		return (0);
-	if (!get_double(info[2], &diameter)
-		|| diameter < 0)
+	pl = (t_plane *)(get_last_node(rt_info->pl)->data);
+	if (!set_coordinate(info[1], &(pl->coordinate)))
 		return (0);
-	if (!set_rgb(info[3], &rgb))
+	if (!set_coordinate(info[2], &(pl->normalized))
+		|| !check_normal(pl->normalized))
 		return (0);
-	sp->coordinate.x = coordinate.x;
-	sp->coordinate.y = coordinate.y;
-	sp->coordinate.z = coordinate.z;
-	sp->diameter = diameter;
-	sp->rgb.r = rgb.r;
-	sp->rgb.g = rgb.g;
-	sp->rgb.b = rgb.b;
+	if (!set_rgb(info[3], &(pl->rgb)))
+		return (0);
 	return (1);
 }
 
-int	set_cylinder(char **info, t_cylinder *cy)// caller must check whether the count of splitted is 6. and this function is called only if splitted[0] is "cy"
+int	set_sphere(char **info, int cnt, t_rt_info *rt_info)
 {
-	t_coordinate	coordinate;
-	t_vector		v;
-	double			diameter;
-	double			height;
-	t_rgb			rgb;
+	double		diameter;
+	t_sphere	*sp;
 
-	if (!set_coordinate(info[1], &coordinate) || !set_coordinate(info[2], &v))
+	if (cnt != 4)
 		return (0);
-	if (!check_normal(v) || !get_double(info[3], &diameter) || diameter < 0)
+	sp = (t_sphere *)(get_last_node(rt_info->sp)->data);
+	if (!set_coordinate(info[1], &(sp->coordinate)))
 		return (0);
-	if (!get_double(info[4], &height) || height < 0 || !set_rgb(info[5], &rgb))
+	if (!get_double(info[2], &diameter) || diameter < 0)
 		return (0);
-	cy->coordinate.x = coordinate.x;
-	cy->coordinate.y = coordinate.y;
-	cy->coordinate.z = coordinate.z;
-	cy->normalized.x = v.x;
-	cy->normalized.y = v.y;
-	cy->normalized.z = v.z;
+	if (!set_rgb(info[3], &(sp->rgb)))
+		return (0);
+	sp->diameter = diameter;
+	return (1);
+}
+
+int	set_cylinder(char **info, int cnt, t_rt_info *rt_info)
+{
+	double		diameter;
+	double		height;
+	t_cylinder	*cy;
+
+	if (cnt != 6)
+		return (0);
+	cy = (t_cylinder *)(get_last_node(rt_info->cy)->data);
+	if (!set_coordinate(info[1], &(cy->coordinate)))
+		return (0);
+	if (!set_coordinate(info[2], &(cy->normalized))
+		|| !check_normal(cy->normalized))
+		return (0);
+	if (!get_double(info[3], &diameter) || diameter < 0)
+		return (0);
+	if (!get_double(info[4], &height) || height < 0)
+		return (0);
+	if (!set_rgb(info[5], &(cy->rgb)))
 	cy->diameter = diameter;
 	cy->height = height;
-	cy->rgb.r = rgb.r;
-	cy->rgb.g = rgb.g;
-	cy->rgb.b = rgb.b;
+	return (1);
+}
+
+int	alloc_sphere(void **ptr){
+	t_sphere	*sp;
+	
+	sp = (t_sphere *)malloc(sizeof(t_sphere));
+	if (!sp)
+		return (0);
+	*ptr = (void *)sp;
+	return (1);
+}
+
+int	alloc_plane(void **ptr){
+	t_plane	*pl;
+	
+	pl = (t_plane *)malloc(sizeof(t_plane));
+	if (!pl)
+		return (0);
+	*ptr = (void *)pl;
+	return (1);
+}
+
+int	alloc_cylinder(void **ptr){
+	t_cylinder	*cy;
+	
+	cy = (t_cylinder *)malloc(sizeof(t_cylinder));
+	if (!cy)
+		return (0);
+	*ptr = (void *)cy;
+	return (1);
+}
+
+int	alloc_new_node(t_node **node, int index)
+{
+	static int	(*allocator[3])(void **)
+		= {alloc_sphere, alloc_plane, alloc_cylinder};
+	
+	*node = (t_node *)malloc(sizeof(t_node));
+	if (!(*node))
+		return (0);
+	if (!(*allocator[index - 3])(&((*node)->data)))
+	{
+		free(*node);
+		return (0);
+	}
+	return (1);
+}
+
+void	append_node(t_node **lst, t_node *new)
+{
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	while ((*lst)->next)
+		lst = &((*lst)->next);
+	(*lst)->next = new;
+}
+
+void	clear_list(t_node **lst) // will be used in error handling function right before exit().
+{
+	t_node	*next;
+
+	while (*lst)
+	{
+		next = (*lst)->next;
+		free((*lst)->data);
+		free(*lst);
+		*lst = next;
+	}
+	*lst = 0;
+}
+
+int	set_object_list(t_rt_info *rt, int index)
+{
+	t_node	**objects;
+	t_node	*new_node;
+
+	if (!alloc_new_node(&new_node, index))
+		return (0);
+	objects = &(rt->sp);
+	if (index == 4)
+		objects = &(rt->pl);
+	else if (index == 5)
+		objects = &(rt->cy);
+	append_node(objects, new_node);
+	return (1);
+}
+
+/*
+splitted
+	1. 널 포인터 및 빈 문자열일 때 : 가드가 거름.
+	2. 내용은 있지만, 유효하지 않은 상태일 때 : 각 set_* 함수들이 걸러야 함.
+*/
+int	set_rt_info(char *line, t_rt_info *rt, int *mask)
+{
+	char		**splitted;
+	int			cnt;
+	int			index;
+	static int	(*fp[6])(char **, int, t_rt_info *) = {set_ambient, set_camera,
+		set_light, set_sphere, set_plane, set_cylinder};
+	
+	splitted = split_line(line, ' ', &cnt); // all split_lines need null-guard.
+	if (!(splitted && splitted[0]))
+		return (free_splitted(splitted, 0));
+	index = check_identifier(splitted[0]);
+	if (index == -1)
+		return (free_splitted(splitted, 0)); // caller must print error if set_rt_info returns 0.
+	if (index < 3 && (*mask & 1 << index || !(*fp[index])(splitted, cnt, rt))) // A, C, L shouldn't be on mutiple lines and don't need malloc.
+		return (free_splitted(splitted, 0));
+	*mask |= 1 << index;
+	// t_node 동적 할당, index에 맞는 오브젝트의 구조체 동적 할당, 할당한 것들을 index에 해당하는 t_rt_info 구조체의 멤버에 할당하는 함수 필요.
+	// set_{sp, pl, cy}에서 t_rt_info 구조체 내의 sp, pl, cy 노드 중 가장 마지막 노드를 가져와서 그 노드의 data에 대해 작업하는 코드 필요.
+	// fp가 0을 반환하면 위에 할당한 것들을 모두 해제하는 함수 필요.
+	// 이 부분에 해당하는 내용을 아예 함수 하나로 빼자.
+	if (2 < index
+		&& (!set_object_list(rt, index)	|| !(*fp[index])(splitted, cnt, rt)))
+		return (free_splitted(splitted, 0));
 	return (1);
 }
 
@@ -291,47 +405,43 @@ int	open_file(char *path)
 	return (fd);
 }
 
+int	read_file(int fd, t_rt_info *rt_info)
+{
+	char		*line;
+	int			flag;
+	static int	mask = 0; // bit-mask to check A, C, L should be declared only and at least once.
+
+	flag = 1;
+	while (flag)
+	{
+		line = get_next_line(fd);
+		if (line == 0)
+			break ;
+		if (line[0] != '\0')
+			flag = set_rt_info(line, rt_info, &mask);
+		free(line);
+	}
+	if (!flag || !(mask & 1 << 0 && mask & 1 << 1 && mask & 1 << 2))
+		return (0);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	// int		fd;
-	// char	*p;
+	int			fd;
+	t_rt_info	rt_info;
 
-	// if (argc != 2)
-	// 	return (0); // call error handling function with proper error message.
-	// fd = open_file(argv[1]);
-	// if (fd < 0) // remove this when error handling function is completed.
-	// 	return (0); // 에러 문자열 출력하고 처리해주기
-	// while (1)
-	// {
-	// 	p = get_next_line(fd);
-	// 	if (p == 0)
-	// 		break ;
-	// 	if (p[0] != '\0')
-	// 		printf("%s\n", p); // 유효성 검사 수행하는 함수 호출
-	// 	free(p);
-	// }
-
-	char		*str1 = "cy 50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255";
-	char		*str2 = "cy 50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255";
-	char		*str3 = "cy 50.0,0.0,20.6 0.0,0.0,1.0 14.2 21.42 10,0,255";
-
-
-	t_cylinder	cy;
-	char		**splitted;
-	int			count;
-
-	splitted = split_line(str1, ' ', &count);
-	if (set_cylinder(splitted, &cy))
-		printf("coordinate x,y,z: %f, %f, %f  normalized x,y,z: %f %f %f \ndiameter: %f height:%f RGB r,g,b: %d, %d, %d\n", cy.coordinate.x, cy.coordinate.y, cy.coordinate.z, cy.normalized.x, cy.normalized.y, cy.normalized.z, cy.diameter, cy.height, cy.rgb.r, cy.rgb.g, cy.rgb.b);
-	free_splitted(splitted,0);
-	splitted = split_line(str2, ' ', &count);
-	if (set_cylinder(splitted, &cy))
-		printf("coordinate x,y,z: %f, %f, %f  normalized x,y,z: %f %f %f \ndiameter: %f height:%f RGB r,g,b: %d, %d, %d\n", cy.coordinate.x, cy.coordinate.y, cy.coordinate.z, cy.normalized.x, cy.normalized.y, cy.normalized.z, cy.diameter, cy.height, cy.rgb.r, cy.rgb.g, cy.rgb.b);
-	free_splitted(splitted,0);
-	splitted = split_line(str3, ' ', &count);
-	if (set_cylinder(splitted, &cy))
-		printf("coordinate x,y,z: %f, %f, %f  normalized x,y,z: %f %f %f \ndiameter: %f height:%f RGB r,g,b: %d, %d, %d\n", cy.coordinate.x, cy.coordinate.y, cy.coordinate.z, cy.normalized.x, cy.normalized.y, cy.normalized.z, cy.diameter, cy.height, cy.rgb.r, cy.rgb.g, cy.rgb.b);
-	free_splitted(splitted,0);
-
+	if (argc != 2)
+		return (0); // call error handling function with proper error message.
+	fd = open_file(argv[1]);
+	if (fd < 0) // remove this when error handling function is completed.
+		return (0); // 에러 문자열 출력하고 처리해주기
+	rt_info.sp = 0;
+	rt_info.pl = 0;
+	rt_info.cy = 0;
+	if (read_file(fd, &rt_info))
+		printf("%s\n", "valid format");
+	else
+		printf("%s\n", "invalid format");
 	return (0);
 }
