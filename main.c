3c73a100317d9394d51c078d67c813402004b210
Eingerjar <cksrb1526@naver.com>
2022-10-18 22:03:22 +0900

    done: get_tanget_norm

--- main.c
+++ main.c
@@ -918,7 +918,9 @@ t_vec	get_ab_vec(t_vec v)
 	return (ret);
 }
 
-void	get_pixel_info(t_camera c, t_p_info *p_info)
+void	
+
+get_pixel_info(t_camera c, t_p_info *p_info)
 {
 	t_vec	h;
 	t_vec	v;
@@ -977,7 +979,17 @@ void	dot_pixel(t_img *img, t_rgb color, int i)
 	img->addr[pixel + 2] = color.r;
 }
 
-t_vec	get_tangent_norm_cy(t_cy *cy, t_coord p)
+t_vec	get_tangent_norm_sp(t_sp *sp, t_coord p, t_vec ray_dir)
+{
+	t_vec	n;
+
+	n = vec_sub(p, sp->coord);
+	if (vec_dot(ray_dir, n) > 0)
+		return (vec_normalize(vec_sub(sp->coord, p)));
+	return (vec_normalize(n));
+}
+
+t_vec	get_tangent_norm_cy(t_cy *cy, t_coord p, t_vec ray_dir)
 {
 	double	a;
 	double	b;
@@ -986,31 +998,76 @@ t_vec	get_tangent_norm_cy(t_cy *cy, t_coord p)
 	a = vec_dot(p, cy->norm);
 	b = vec_dot(vec_add(cy->coord, vec_scale(cy->norm, cy->height)), cy->norm);
 	if (fabs(a - b) < 1e-6)
+	{
+		if (vec_dot(ray_dir, cy->norm) > 0)
+			return (vec_scale(cy->norm, -1));
 		return (cy->norm);
+	}
 	b = vec_dot(cy->coord, cy->norm);
 	if (fabs(a - b) < 1e-6)
+	{
+		if (vec_dot(ray_dir, cy->norm) > 0)
+			return (cy->norm);
 		return (vec_scale(cy->norm, -1));
+	}
 	t_c = vec_add(cy->coord, vec_scale(cy->norm, fabs(a - b)));
+	if (vec_dot(ray_dir, vec_sub(p, t_c)) > 0)
+		return (vec_normalize(vec_sub(t_c, p)));
 	return (vec_normalize(vec_sub(p, t_c)));
 }
 
-t_vec	get_tangent_norm(t_obj	obj, t_coord p)
+t_vec	get_tangent_norm_pl(t_pl *pl, t_vec ray_dir)
+{
+	if (vec_dot(ray_dir, pl->norm) > 0)
+		return (vec_scale(pl->norm, -1));
+	return (pl->norm);
+}
+
+// t_vec	get_tangent_norm_cn(t_cn *cn, t_coord p, t_vec ray_dir)
+// {
+// 	double	a;
+// 	double	b;
+// 	t_coord	t_c;
+
+// 	a = vec_dot(p, cn->norm);
+// 	b = vec_dot(vec_add(cn->coord, vec_scale(cn->norm, cn->height)), cn->norm);
+// 	if (fabs(a - b) < 1e-6)
+// 	{
+// 		if (vec_dot(ray_dir, cn->norm) > 0)
+// 			return (vec_scale(cn->norm, -1));
+// 		return (cn->norm);
+// 	}
+// 	b = vec_dot(cn->coord, cn->norm);
+// 	if (fabs(a - b) < 1e-6)
+// 	{
+// 		if (vec_dot(ray_dir, cn->norm) > 0)
+// 			return (cn->norm);
+// 		return (vec_scale(cn->norm, -1));
+// 	}
+// 	t_c = vec_add(cn->coord, vec_scale(cn->norm, fabs(a - b)));
+// 	if (vec_dot(ray_dir, vec_sub(p, t_c)) > 0)
+// 		return (vec_normalize(vec_sub(t_c, p)));
+// 	return (vec_normalize(vec_sub(p, t_c)));
+// }
+
+t_vec	get_tangent_norm(t_obj	obj, t_coord p, t_vec ray_dir)
 {
 	t_vec	n;
 
 	if (obj.type == SPHERE)
-		n = vec_normalize(vec_sub(p, ((t_sp *)obj.object)->coord));
+		n = get_tangent_norm_sp(obj.object, p, ray_dir);
 	else if (obj.type == CYLINDER)
-		n = get_tangent_norm_cy(obj.object, p);
-	else
-		n = ((t_pl *)obj.object)->norm;
+		n = get_tangent_norm_cy(obj.object, p, ray_dir);
+	else if (obj.type == PLANE)
+		n = get_tangent_norm_pl(obj.object, ray_dir);
+	// else if (obj.type == CONE)
+	// 	n = get_tangent_norm_cn(obj.object, p, ray_dir);
 	return (n);
 }
 
 int	trace_ray(t_img *img, t_world *world, t_ray ray, int i)
 {
 	t_obj	obj;
-	// t_ray	l_ray;
 	t_rgb	color;
 	t_coord	p;
 	t_vec	n;
@@ -1019,13 +1076,7 @@ int	trace_ray(t_img *img, t_world *world, t_ray ray, int i)
 		return (0);
 	//background color
 	p = vec_add(ray.pos, vec_scale(ray.dir, obj.t));
-	n = get_tangent_norm(obj, p);
-	// l_ray = get_l_ray(world->l, ray, obj);
-	// if (check_shadow(*world, l_ray))
-	// {
-	// 	//default
-	// 	return (0);
-	// }
+	n = get_tangent_norm(obj, p, ray.dir);
 	color = get_obj_rgb(obj,
 		compute_lighting(p, n, vec_neg(vec_normalize(ray.dir)), *world));
 	dot_pixel(img, color, i);
